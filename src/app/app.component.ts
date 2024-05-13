c4b85ffe86f3fef7eb37ac88879a9dceda611c78
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-05-13 11:33:27 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Persona } from './persona';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  dia?: string;
   title = 'lab10';
 }
