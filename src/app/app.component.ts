8e578ca8f6eb1a5241e4e949a63664dbf47e68f0
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-05-20 11:05:07 -0500

    Desarrollo de laboratorio semana 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app11';
+  nombre = '';
 }
